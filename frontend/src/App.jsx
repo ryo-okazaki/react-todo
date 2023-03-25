e712535e8beb280a4aace0e76b3f789010a88fed
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-25 16:02:50 +0900

    完了タスクも同様の修正

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 export const App = () => {
   const [incompleteTodos, setIncompleteTodos] = useState(['ああああ', 'いいいい']);
 
+  const [completeTodos, setCompleteTodos] = useState(['うううう'])
+
   return (
     <>
       <div className="input-area">
@@ -31,14 +33,14 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>うううう</li>
-            <button>戻す</button>
-          </div>
-          <div className="list-row">
-            <li>いいいい</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>うううう</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
