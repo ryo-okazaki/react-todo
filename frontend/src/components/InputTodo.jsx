2d7a5673ef444e103d82b1ae82db240fa011a810
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-25 20:36:39 +0900

    CSS-in-js

--- frontend/src/components/InputTodo.jsx
+++ frontend/src/components/InputTodo.jsx
@@ -3,9 +3,18 @@ import React from "react";
 export const InputTodo = (props) => {
   const {todoText, onChange, onClick} = props;
 
+  const style = {
+  backgroundColor: '#c1ffff',
+  width: '400px',
+  height: '30px',
+  padding: '8px',
+  margin: '8px',
+  borderRadius: '8px',
+  }
+
   return (
     <>
-      <div className="input-area">
+      <div style={style}>
         <input
           placeholder="TODOを入力"
           value={todoText}
